c80f33b8d98868c7ce213a0cca6aeea4760c3336
James Birchall <jamesbirchall@gmail.com>
2017-08-07 21:16:53 +0100

    updates
    Added connections, custom tableviewcell and started modifying the view
    controller to act as data source and delegate for the table inside it.

---
+++ JustMyStuff/JustMyStuff/ItemTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemTableViewCell.swift
+//  JustMyStuff
+//
+//  Created by James Birchall on 07/08/2017.
+//  Copyright © 2017 James Birchall. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var detailsLabel: UILabel!
+    
+}
--- JustMyStuff/JustMyStuff/ViewController.swift
+++ JustMyStuff/JustMyStuff/ViewController.swift
@@ -8,12 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    // MARK: - Outlets
+    
+    @IBOutlet weak var sortingSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var itemTableView: UITableView!
+    
+    // MARK: - Overrides for ViewController
+    
     override func viewDidLoad() {
         super.viewDidLoad()
      
-        
+        itemTableView.delegate = self
+        itemTableView.dataSource = self
     }
+    
+    // MARK: - UITableViewDataSource Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if let cell = itemTableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath) as? ItemTableViewCell {
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
+    // MARK: - UITableViewDelegate Methods
 }
 
